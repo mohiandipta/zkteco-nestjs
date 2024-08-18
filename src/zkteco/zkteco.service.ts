b8026f00abcc8b98db64b6cb638a87f2683ca74c
mohiandipta <mohianmostofa@gmail.com>
2024-08-18 17:57:49 +0600

    successfully fetched data

--- src/zkteco/zkteco.service.ts
+++ src/zkteco/zkteco.service.ts
@@ -1,30 +1,49 @@
 import { Injectable, Logger } from '@nestjs/common';
 const ZKJUBAER = require("zk-jubaer");
+const ZKLib = require('node-zklib');
 
 @Injectable()
 export class ZktecoService {
     private readonly logger = new Logger(ZktecoService.name);
 
+    // async getData() {
+    //     console.log("Get Data")
+    //     let zkInstance = new ZKLib("10.38.60.73", 4370, 10000, 4000);
+    //     try {
+    //         await zkInstance.createSocket();
+
+    //         const users = await zkInstance.getAttendances();
+    //         console.log(users);
+
+    //         await zkInstance.disconnect();
+    //     } catch (error) {
+    //         console.log(error)
+    //         this.logger.error(`Error fetching data from ZKTeco device: ${error.message}`);
+    //         throw new Error(`Error fetching data from ZKTeco device: ${error.message}`);
+    //     }
+    // }
+
     async getData() {
         console.log("Get Data")
-        let obj = new ZKJUBAER("10.38.61.14", 4370, 50000, 5000);
+        let obj = new ZKJUBAER("10.38.60.70", 4370, 10000, 4000);
         try {
             // Create socket to machine
             await obj.createSocket()
-            .then((result) => {
-                console.log(result)
-            }).catch((err) => {
-                console.log(err)
-            });
+
+            const users = await obj.getUsers()
 
             // Get all logs in the machine
-            const logs = await obj.getAttendances();
-            console.log(logs);
+            const attendances = await obj.getAttendances();
 
             // Read real-time logs
             await obj.getRealTimeLogs((data: any) => {
                 console.log(data);
             });
+
+            return {
+                "users": users,
+                "attendances": attendances
+            }
 
             // Disconnect from device
             await obj.disconnect(); // when you are using real-time logs, you need to disconnect manually
